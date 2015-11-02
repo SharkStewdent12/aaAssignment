b6ffd6a9b7ea71eff09fc39749d0f921e3e3154a
SharkStewdent12 <570169@students.wits.ac.za>
2015-11-02 08:35:22 +0200

    Prepare for merge

--- workspace/AVLTrees/src/AvlTree.java
+++ workspace/AVLTrees/src/AvlTree.java
@@ -30,14 +30,14 @@ public class AvlTree {
 //		tree.insert(25);
 //		tree.insert(29);	
 		
-		tree.insert(2);
-		tree.insert(1);
-		tree.insert(2);
-		tree.insert(3);
-		tree.insert(1);
-		tree.insert(4);
-		tree.insert(4);
-		tree.insert(2);
+//		tree.insert(2);
+//		tree.insert(1);
+//		tree.insert(2);
+//		tree.insert(3);
+//		tree.insert(1);
+//		tree.insert(4);
+//		tree.insert(4);
+//		tree.insert(2);
 		
 
 
