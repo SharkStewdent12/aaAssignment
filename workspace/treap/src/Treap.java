001791c334ba1a629871e72b9636b75fe4c9fc86
jammysmcd <jammysmcd@outlook.com>
2015-11-02 04:17:54 +0200

    treap update1

--- workspace/treap/src/Treap.java
+++ workspace/treap/src/Treap.java
@@ -21,123 +21,114 @@ import java.util.Random;
  * Note that all "matching" is based on the compareTo method.
  * @author Mark Allen Weiss
  */
-public class Treap<AnyType extends Comparable<? super AnyType>>
+public class Treap
 {
 	/**
 	 * Construct the treap.
 	 */
-	public Treap( )
-	{
-		nullNode = new TreapNode<AnyType>( null );
-		nullNode.left = nullNode.right = nullNode;
-		nullNode.priority = Integer.MAX_VALUE;
-		root = nullNode;
-	}
+//	public Treap( )
+//	{
+//		nullNode = new TreapNode( null );
+//		nullNode.leftChild = nullNode.rightChild = nullNode;
+//		nullNode.priority = Integer.MAX_VALUE;
+//		root = nullNode;
+//	}
 
 	/**
 	 * Insert into the tree. Does nothing if x is already present.
 	 * @param x the item to insert.
 	 */
-	public void insert( AnyType x )
+	public void insert(char k)
 	{
-		root = insert( x, root );
+		TreapNode x=new TreapNode(k);
+		BinaryInsert(this.root,x);
+		while (x.parent != null && x.priority > x.parent.priority){
+			if (x.parent.rightChild == x){
+				rotateLeft(x.parent);
+			}else{
+		    	rotateRight(x.parent);
+			}
+		}
 	}
 
-	/**
-	 * Remove from the tree. Does nothing if x is not found.
-	 * @param x the item to remove.
-	 */
-	public void remove( AnyType x )
-	{
-		root = remove( x, root );
+	public TreapNode rotateLeft(TreapNode n) {
+		TreapNode v = n.rightChild;
+		v.parent = n.parent;
+	
+		n.rightChild = v.leftChild;
+	
+		if(n.rightChild!=null) {
+			n.rightChild.parent=n;
+		}
+	
+		v.leftChild = n;
+		n.parent = v;
+	
+		if(v.parent!=null) {
+			if(v.parent.rightChild==n) {
+				v.parent.rightChild = v;
+			} else if(v.parent.leftChild==n) {
+				v.parent.leftChild = v;
+			}
+		}
+		return v;
 	}
 
-	/**
-	 * Find the smallest item in the tree.
-	 * @return the smallest item, or throw UnderflowException if empty.
-	 * @throws Exception 
-	 */
-	public AnyType findMin( ) throws Exception
-	{
-		if( isEmpty( ) )
-			throw new Exception( );
+	public TreapNode rotateRight(TreapNode n) {
 
-		TreapNode<AnyType> ptr = root;
+		TreapNode v = n.leftChild;
+		v.parent = n.parent;
 
-		while( ptr.left != nullNode )
-			ptr = ptr.left;
+		n.leftChild = v.rightChild;
 
-		return ptr.element;
+		if(n.leftChild!=null) {
+			n.leftChild.parent=n;
+		}
+
+		v.rightChild = n;
+		n.parent = v;
+
+
+		if(v.parent!=null) {
+			if(v.parent.rightChild==n) {
+				v.parent.rightChild = v;
+			} else if(v.parent.leftChild==n) {
+				v.parent.leftChild = v;
+			}
+		}
+
+
+		return v;
 	}
-
-	/**
-	 * Find the largest item in the tree.
-	 * @return the largest item, or throw UnderflowException if empty.
-	 * @throws Exception 
-	 */
-	public AnyType findMax( ) throws Exception
-	{
-		if( isEmpty( ) )
-			throw new Exception( );
-
-		TreapNode<AnyType> ptr = root;
-
-		while( ptr.right != nullNode )
-			ptr = ptr.right;
-
-		return ptr.element;
-	}
-
 	/**
 	 * Find an item in the tree.
 	 * @param x the item to search for.
 	 * @return true if x is found.
 	 */
-	public boolean contains( AnyType x )
-	{
-		TreapNode<AnyType> current = root;
-		nullNode.element = x;
-
-		for( ; ; )
-		{
-			int compareResult = x.compareTo( current.element );
-
-			if( compareResult < 0 )
-				current = current.left;
-			else if( compareResult > 0 ) 
-				current = current.right;
-			else
-				return current != nullNode;
-		}
-	}
-
-	/**
-	 * Make the tree logically empty.
-	 */
-	public void makeEmpty( )
-	{
-		root = nullNode;
-	}
-
-	/**
-	 * Test if the tree is logically empty.
-	 * @return true if empty, false otherwise.
-	 */
-	public boolean isEmpty( )
-	{
-		return root == nullNode;
-	}
-
+//	public boolean contains(  x )
+//	{
+//		TreapNode current = root;
+//		nullNode.element = x;
+//
+//		for( ; ; )
+//		{
+//			int compareResult = x.compareTo( current.element );
+//
+//			if( compareResult < 0 )
+//				current = current.left;
+//			else if( compareResult > 0 ) 
+//				current = current.right;
+//			else
+//				return current != nullNode;
+//		}
+//	}
 	/**
 	 * Print the tree contents in sorted order.
 	 */
-	public void printTree( )
-	{
-		if( isEmpty( ) )
-			System.out.println( "Empty tree" );
-		else
-			printTree( root );
-	}
+//	public void printTree( )
+//	{
+//			printTree( root );
+//	}
 
 	/**
 	 * Internal method to insert into a subtree.
@@ -145,29 +136,29 @@ public class Treap<AnyType extends Comparable<? super AnyType>>
 	 * @param t the node that roots the subtree.
 	 * @return the new root of the subtree.
 	 */
-	private TreapNode<AnyType> insert( AnyType x, TreapNode<AnyType> t )
-	{
-		if( t == nullNode )
-			return new TreapNode<AnyType>( x, nullNode, nullNode );
-
-		int compareResult = x.compareTo( t.element );
-
-		if( compareResult < 0 )
-		{
-			t.left = insert( x, t.left );
-			if( t.left.priority < t.priority )
-				t = rotateWithLeftChild( t );
-		}
-		else if( compareResult > 0  )
-		{
-			t.right = insert( x, t.right );
-			if( t.right.priority < t.priority )
-				t = rotateWithRightChild( t );
-		}
-		// Otherwise, it's a duplicate; do nothing
-
-		return t;
-	}
+//	private TreapNode insert( AnyType x, TreapNode t )
+//	{
+//		if( t == nullNode )
+//			return new TreapNode<AnyType>( x, nullNode, nullNode );
+//
+//		int compareResult = x.compareTo( t.element );
+//
+//		if( compareResult < 0 )
+//		{
+//			t.left = insert( x, t.left );
+//			if( t.left.priority < t.priority )
+//				t = rotateWithLeftChild( t );
+//		}
+//		else if( compareResult > 0  )
+//		{
+//			t.right = insert( x, t.right );
+//			if( t.right.priority < t.priority )
+//				t = rotateWithRightChild( t );
+//		}
+//		// Otherwise, it's a duplicate; do nothing
+//
+//		return t;
+//	}
 
 	/**
 	 * Internal method to remove from a subtree.
@@ -175,126 +166,97 @@ public class Treap<AnyType extends Comparable<? super AnyType>>
 	 * @param t the node that roots the subtree.
 	 * @return the new root of the subtree.
 	 */
-	private TreapNode<AnyType> remove( AnyType x, TreapNode<AnyType> t )
-	{
-		if( t != nullNode )
-		{
-			int compareResult = x.compareTo( t.element );
 
-			if( compareResult < 0 )
-				t.left = remove( x, t.left );
-			else if( compareResult > 0 )
-				t.right = remove( x, t.right );
-			else
-			{
-				// Match found
-				if( t.left.priority < t.right.priority )
-					t = rotateWithLeftChild( t );
-				else
-					t = rotateWithRightChild( t );
-
-				if( t != nullNode )     // Continue on down
-					t = remove( x, t );
-				else
-					t.left = nullNode;  // At a leaf
-			}
-		}
-		return t;
-	}
 
 	/**
 	 * Internal method to print a subtree in sorted order.
 	 * @param t the node that roots the tree.
 	 */
-	private void printTree( TreapNode<AnyType> t )
+	private void printTree( TreapNode t )
 	{
-		if( t != t.left )
+		System.out.println( t.key );
+		if(t.leftChild!=null )
 		{
-			printTree( t.left );
-			System.out.println( t.element.toString( ) );
-			printTree( t.right );
+			printTree( t.leftChild );
+		}
+		if(t.rightChild!=null )
+		{
+			printTree( t.rightChild );
+		}
+	}
+
+	public void BinaryInsert(TreapNode subtreeRoot, TreapNode newNode) {
+		// If  node to compare is null, the node is inserted. If the root is null, it is the root of the tree.
+		if(subtreeRoot==null) {
+			this.root=newNode;
+		} else {
+
+			// If compare node is smaller, continue with the left node
+			if(newNode.key<subtreeRoot.key) {
+				if(subtreeRoot.leftChild==null) {
+					subtreeRoot.leftChild = newNode;
+					newNode.parent = subtreeRoot;
+
+				} else {
+					BinaryInsert(subtreeRoot.leftChild,newNode);
+				}
+
+			} else if(newNode.key>subtreeRoot.key) {
+				if(subtreeRoot.rightChild==null) {
+					subtreeRoot.rightChild = newNode;
+					newNode.parent = subtreeRoot;
+
+				} else {
+					BinaryInsert(subtreeRoot.rightChild,newNode);
+				}
+			} else {
+				// do nothing: This node already exists
+			}
 		}
 	}
 
 	/**
 	 * Rotate binary tree node with left child.
 	 */
-	private TreapNode<AnyType> rotateWithLeftChild( TreapNode<AnyType> k2 )
+	private TreapNode leftRotate( TreapNode k2 )
 	{
-		TreapNode<AnyType> k1 = k2.left;
-		k2.left = k1.right;
-		k1.right = k2;
+		TreapNode k1 = k2.leftChild;
+		k2.leftChild = k1.rightChild;
+		k1.rightChild = k2;
 		return k1;
 	}
 
 	/**
 	 * Rotate binary tree node with right child.
 	 */
-	private TreapNode<AnyType> rotateWithRightChild( TreapNode<AnyType> k1 )
+	private TreapNode rightRotate( TreapNode k1 )
 	{
-		TreapNode<AnyType> k2 = k1.right;
-		k1.right = k2.left;
-		k2.left = k1;
+		TreapNode k2 = k1.rightChild;
+		k1.rightChild = k2.leftChild;
+		k2.leftChild = k1;
 		return k2;
 	}
-
-	private static class TreapNode<AnyType>
-	{
-		// Constructors
-		TreapNode( AnyType theElement )
-		{
-			this( theElement, null, null );
-		}
-
-		TreapNode( AnyType theElement, TreapNode<AnyType> lt, TreapNode<AnyType> rt )
-		{
-			element  = theElement;
-			left     = lt;
-			right    = rt;
-			priority = randomObj.nextInt();
-		}
-
-		// Friendly data; accessible by other package routines
-		AnyType            element;      // The data in the node
-		TreapNode<AnyType> left;         // Left child
-		TreapNode<AnyType> right;        // Right child
-		int                priority;     // Priority
-
-		private static Random randomObj = new Random( );
-	}
-
-	private TreapNode<AnyType> root;
-	private TreapNode<AnyType> nullNode;
+	
+	private TreapNode root;
+	private TreapNode nullNode;
 
 
 	// Test program
 	public static void main( String [ ] args ) throws Exception
 	{
-		Treap<Integer> t = new Treap<Integer>( );
-		final int NUMS = 40000;
-		final int GAP  =   307;
+		Treap tree = new Treap( );
 
-		System.out.println( "Checking... (no bad output means success)" );
-
-		for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-			t.insert( i );
+		tree.insert('E');
+		tree.insert('A');
+		tree.insert('D');
+		tree.insert('F');
+		tree.insert('B');
+		tree.insert('I');
+		tree.insert('K');
+		tree.insert('L');
+		tree.insert('Z');
+		
 		System.out.println( "Inserts complete" );
-
-		for( int i = 1; i < NUMS; i+= 2 )
-			t.remove( i );
-		System.out.println( "Removes complete" );
-
-		if( NUMS < 40 )
-			t.printTree( );
-		if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 )
-			System.out.println( "FindMin or FindMax error!" );
-
-		for( int i = 2; i < NUMS; i+=2 )
-			if( !t.contains( i ) )
-				System.out.println( "Error: find fails for " + i );
-
-		for( int i = 1; i < NUMS; i+=2 )
-			if( t.contains( i ) )
-				System.out.println( "Error: Found deleted item " + i );
+		tree.printTree(tree.root);
 	}
 }
